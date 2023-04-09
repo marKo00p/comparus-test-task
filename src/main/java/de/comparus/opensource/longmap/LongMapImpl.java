4eb24a912a6b2dc9763b77d54a1d1e5f1b02f1ac
Yuliia Semykina <semykinayuliia@gmail.com>
2023-04-09 18:47:36 +0300

    add changes to LongMapImpl and LongMapImplTest classes

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,212 @@
 package de.comparus.opensource.longmap;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class LongMapImpl<V> implements LongMap<V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private int capacity;
+    private final float loadFactor;
+    private Node<V>[] table;
+    public LongMapImpl() {
+        this(DEFAULT_CAPACITY);
+    }
+    @SuppressWarnings("unchecked")
+    public LongMapImpl(int initialCapacity) {
+        verifyCapacity(initialCapacity);
+        this.capacity = initialCapacity;
+        this.table = (Node<V>[]) new Node[capacity];
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+    }
+    @SuppressWarnings("unchecked")
+    public LongMapImpl(int initialCapacity, float loadFactor) {
+        verifyCapacity(initialCapacity);
+        this.loadFactor = loadFactor;
+        this.table =(Node<V>[])  new Node[capacity];
+    }
+
+    private void verifyCapacity(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity (table array size) must be positive");
+        }
+    }
     public V put(long key, V value) {
+        int index = hash(key);
+        Node<V> node = table[index];
+        if (node == null) {
+            table[index] = new Node<>(key, value);
+            size++;
+        } else {
+            Optional<Node<V>> existingNode = Stream.iterate(node, Objects::nonNull, n -> n.next)
+                    .filter(n -> n.key == key)
+                    .findFirst();
+            if (existingNode.isPresent()) {
+                existingNode.get().value = value;
+                return value;
+            } else {
+                Optional<Node<V>> optionalLastNode = Stream.iterate(node, Objects::nonNull, n -> n.next)
+                        .reduce((a, b) -> b);
+                if (optionalLastNode.isPresent()) {
+                    Node<V> lastNode = optionalLastNode.get();
+                    lastNode.next = new Node<>(key, value);
+                    size++;
+                }
+            }
+        }
+        resizeIfNeeded();
         return null;
     }
 
+
     public V get(long key) {
-        return null;
+        Node<V> newNode = table[hash(key)];
+        return Stream.iterate(newNode, Objects::nonNull, Node::getNext)
+                .filter(node -> node.getKey() == key)
+                .map(Node::getValue)
+                .findFirst()
+                .orElse(null);
     }
 
     public V remove(long key) {
+        int index = hash(key);
+        Node<V> node = table[index];
+        if (node == null) {
+            return null;
+        }
+        if (node.key == key) {
+            table[index] = node.next;
+            size--;
+            return node.value;
+        }
+        Node<V> prev = node;
+        node = node.next;
+        Optional<Node<V>> optional = Stream.iterate(node, Objects::nonNull, Node::getNext)
+                .filter(n -> n.key == key)
+                .findFirst();
+        if (optional.isPresent()) {
+            Node<V> found = optional.get();
+            prev.setNext(found.getNext());
+            size--;
+            return found.getValue();
+        }
         return null;
     }
 
+
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean containsKey(long key) {
-        return false;
+        int index = hash(key);
+        return Arrays.stream(new Node[] {table[index]})
+                .anyMatch(node -> node != null && node.getKey() == key);
     }
 
     public boolean containsValue(V value) {
-        return false;
+        return Arrays.stream(table)
+                .filter(Objects::nonNull)
+                .flatMap(node -> Stream.iterate(node, Objects::nonNull, Node::getNext))
+                .anyMatch(node -> node.getValue().equals(value));
     }
 
     public long[] keys() {
-        return null;
+        return Arrays.stream(table)
+                .filter(Objects::nonNull)
+                .flatMap(node -> Stream.iterate(node, Objects::nonNull, Node::getNext))
+                .mapToLong(Node::getKey)
+                .toArray();
     }
 
+    @SuppressWarnings("unchecked")
     public V[] values() {
-        return null;
+        return Arrays.stream(table)
+                .filter(Objects::nonNull)
+                .flatMap(node -> Stream.iterate(node, Objects::nonNull, Node::getNext))
+                .map(Node::getValue)
+                .toArray(size -> (V[]) new Object[size]);
     }
 
     public long size() {
-        return 0;
+        return size;
     }
 
     public void clear() {
+        size = 0;
+        Arrays.stream(table).forEach(node -> node = null);
+    }
+    private int hash(long key) {
+        return (int) key % capacity;
+    }
 
+    private void resizeIfNeeded() {
+        float load = (float) size / capacity;
+        if (load > loadFactor) {
+            capacity *= 2;
+            Node<V>[] oldTable = table;
+            table =  new Node[capacity];
+            size = 0;
+            Arrays.stream(oldTable)
+                    .filter(Objects::nonNull)
+                    .flatMap(n -> Stream.iterate(n, Objects::nonNull, x -> x.next))
+                    .forEach(n -> put(n.key, n.value));
+        }
+    }
+
+
+    static class Node<V> {
+        private final long key;
+        private V value;
+        private Node<V> next;
+
+        public Node(long key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
+        public long getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Node<V> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<V> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "key = " + key +
+                    " value = " + value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Node)) return false;
+            Node<?> node = (Node<?>) o;
+            return key == node.key && value.equals(node.value) && Objects.equals(next, node.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value, next);
+        }
     }
 }
---
+++ src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -0,0 +1,182 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@DisplayName("LongMapImpl Test")
+class LongMapImplTest {
+private LongMapImpl<String> longMap = new LongMapImpl<>();
+    private LongMapImpl.Node<?>[] getInternalTable(LongMapImpl<?> map) throws Exception {
+        Field tableField = LongMapImpl.class.getDeclaredField("table");
+        tableField.setAccessible(true);
+        return (LongMapImpl.Node<?>[]) tableField.get(map);
+    }
+
+    @BeforeEach
+    void setUp() {
+        longMap.put(833,"hashMap");
+        longMap.put(33,"linkedList");
+        longMap.put(333,"arrayList");
+    }
+
+    @AfterEach
+    void tearDown() {
+        longMap.clear();
+    }
+    @Test
+    @DisplayName("LongMapImpl has a field 'table' which is an array of nodes")
+    public void testGetInternalTable() throws Exception {
+        LongMapImpl<String> map = new LongMapImpl<>();
+        LongMapImpl.Node<?>[] table = getInternalTable(map);
+        assertNotNull(table);
+    }
+    @Test
+    @DisplayName("A nested class Node exists")
+    void nodeClassExists() {
+        List<String> nestedClassList = Arrays.stream(LongMapImpl.class.getDeclaredClasses())
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
+
+        assertAll("Nested classes",
+                () -> assertTrue(nestedClassList.contains("Node"))
+        );
+    }
+    @Test
+    @DisplayName("A default constructor capacity")
+    void  defaultConstructor() throws Exception {
+        Constructor<?> initialConstructor = LongMapImpl.class.getDeclaredConstructor();
+        initialConstructor.setAccessible(true);
+        LongMapImpl<?> longMap = (LongMapImpl<?>) initialConstructor.newInstance();
+        LongMapImpl.Node<?>[] table = getInternalTable(longMap);
+
+        assertEquals(16, table.length);
+    }
+
+    @Test
+    @DisplayName("An additional constructor accepts an initial array size")
+    void constructorWithTableCapacity() throws Exception{
+        Constructor<?> constructor = LongMapImpl.class.getConstructor(int.class);
+
+        LongMapImpl<?> longMap = (LongMapImpl<?>) constructor.newInstance(32);
+        LongMapImpl.Node<?>[] table = getInternalTable(longMap);
+
+        assertEquals(32, table.length);
+    }
+
+    @Test
+    @DisplayName("An element with same key")
+    void putElementWithTheSameKey() {
+        assertEquals(longMap.get(833),"hashMap");
+        longMap.put(833,"dequeue");
+        assertEquals(longMap.get(833),"dequeue");
+    }
+
+    @Test
+    @DisplayName("An million element")
+    void putMillionElements() {
+        int millionOfElements = 1_000_000;
+        longMap = new LongMapImpl<>(millionOfElements);
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < millionOfElements; i++) {
+            longMap.put(i, "value");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(longMap.size());
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+    @Test
+    @DisplayName("Doesn't exists key")
+    void getElementWhenKeyDoesNotExists() {
+        Object foundValue = longMap.get(2);
+        assertNull(foundValue);
+    }
+    @Test
+    @DisplayName("Get returns a corresponding value by the given key")
+    void get() {
+        assertEquals("arrayList", longMap.get(333));
+    }
+    @Test
+    @DisplayName("remove element")
+    void remove() {
+        assertEquals(3, longMap.size());
+        longMap.remove(833);
+        assertEquals(2, longMap.size());
+    }
+    @Test
+    @DisplayName("returns null if key doesn't exists")
+    void removeWhenKeyDoesNotExists() {
+        Object result = longMap.remove(233);
+        assertNull(result);
+    }
+    @Test
+    @DisplayName("returns false when there are some elements")
+    void isEmpty() {
+        assertFalse(longMap.isEmpty());
+    }
+    @Test
+    @DisplayName("returns true if it doesn't contain elements")
+    void isEmptyWhenThereIsNoElements() {
+        longMap.clear();
+        assertTrue(longMap.isEmpty());
+    }
+
+    @Test
+    @DisplayName("returns true if key exists")
+    void containsKey() {
+        assertTrue(longMap.containsKey(833));
+    }
+    @Test
+    @DisplayName("returns false if key doesn't exists")
+    void doesnt_containsKey() {
+        assertFalse(longMap.containsKey(3));
+    }
+    @Test
+    @DisplayName("returns true if value exists")
+    void containsValue() {
+        assertTrue(longMap.containsValue("arrayList"));
+    }
+    @Test
+    @DisplayName("returns false if value doesn't exists")
+    void doesnt_containsValue() {
+        assertFalse(longMap.containsValue("list"));
+    }
+    @Test
+    @DisplayName("returns null if value doesn't exists")
+    void containsValueWhenItDoesNotExist() {
+        assertFalse(longMap.containsValue("666"));
+    }
+    @Test
+    @DisplayName("returns all keys")
+    void keys() {
+        long[] expected = {833, 33, 333};
+        assertArrayEquals(expected, longMap.keys());
+    }
+    @Test
+    @DisplayName("returns all values")
+    void values() {
+        String[] expected = {"hashMap", "linkedList", "arrayList"};
+        assertArrayEquals(expected, longMap.values());
+    }
+    @Test
+    @DisplayName("size returns the number of nodes in the table")
+    void size() {
+        assertEquals(longMap.size(), 3);
+    }
+
+    @Test
+    @DisplayName("delete all elements")
+    void clear() {
+        assertEquals(3, longMap.size());
+        longMap.clear();
+        assertTrue(longMap.isEmpty());
+        assertEquals(0, longMap.size());
+    }
+}
